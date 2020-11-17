053c88b2afc072109bfe7aede0e00f8c455e391a
Janos Nagy <janosnagy0212@gmail.com>
2020-11-17 23:17:34 +0000

    WIP: input validation

--- src/com/tictactoe/Game.java
+++ src/com/tictactoe/Game.java
@@ -15,7 +15,8 @@ public class Game {
     public Game(){
         initializeGame();
         displayBoard();
-        makeFirstMove();
+        askQuestion();
+        //computerFirstMove();
         playGame();
         checkStatus();
     }
@@ -30,17 +31,35 @@ public class Game {
         board.displayBoard();
     }
 
-    private void makeFirstMove() {
+    private void askQuestion(){
         System.out.println("Who would you like to make the first move?");
         System.out.println("1 - Computer");
         System.out.println("2 - Player");
         int choice = board.getScanner().nextInt();
-        if(choice == 1){
-            //random.nextInt(Constants.BOARD_SIZE) -> random number between 0 and boardSize
-            Tile tile = new Tile(random.nextInt(Constants.BOARD_SIZE), random.nextInt(Constants.BOARD_SIZE));
-            board.move(tile, Player.COMPUTER);
-            board.displayBoard();
+        if(choice == 1) {
+            computerFirstMove();
         }
+        if(!(choice == 1) && !(choice == 2)){
+            wrongPick();
+        }
+    }
+
+    private void wrongPick(){
+        System.out.println("Please enter: \n1 for Computer or \n2 for Player.");
+        int choice = board.getScanner().nextInt();
+        if(choice == 1) {
+            computerFirstMove();
+        }
+        if(!(choice == 1) && !(choice == 2)){
+            wrongPick();
+        }
+    }
+
+    private void computerFirstMove() {
+        //random.nextInt(Constants.BOARD_SIZE) -> random number between 0 and boardSize
+        Tile tile = new Tile(random.nextInt(Constants.BOARD_SIZE), random.nextInt(Constants.BOARD_SIZE));
+        board.move(tile, Player.COMPUTER);
+        board.displayBoard();
     }
 
     private void playGame(){
@@ -48,7 +67,7 @@ public class Game {
         int columnInt = -1;
         int rowInt = -1;
         while (board.isRunning()){
-            System.out.println( "Make a move by entering the letter corresponding to your chosen columnChar plus enter\n" +
+            System.out.println( "Make a move by entering the letter corresponding to your chosen column plus enter\n" +
                                 "and the corresponding number to your chosen row plus enter.\n" +
                                 "Please enter your move:");
             columnChar = board.getScanner().next().charAt(0);
@@ -67,10 +86,12 @@ public class Game {
             for (Tile tile : board.getRootValues()) {
                 System.out.println("Tile values: " + tile + " minimaxValue: " + tile.getMinimaxValue());
             }
-            board.move(board.getBestMove(), Player.COMPUTER);
-            board.displayBoard();
-            if (!board.isRunning()) {
-                break;
+            if(board.getEmptyTiles().size() != 0) {
+                board.move(board.getBestMove(), Player.COMPUTER);
+                board.displayBoard();
+                if (!board.isRunning()) {
+                    break;
+                }
             }
         }
     }
