63a409e91b655f0b3adbda25f65697094f5172d8
Janos Nagy <janosnagy0212@gmail.com>
2020-11-19 23:53:39 +0000

    WIP: validation of user input

--- src/com/tictactoe/Game.java
+++ src/com/tictactoe/Game.java
@@ -3,9 +3,9 @@ package com.tictactoe;
 import java.util.Random;
 
 public class Game {
-
     private Board board;
     private Random random;
+    private boolean flag = false;
 
     public Game(){
         initializeGame();
@@ -34,18 +34,18 @@ public class Game {
             computerFirstMove();
         }
         if(!(choice == 1) && !(choice == 2)){
-            wrongPick();
+            getNewChoiceWhoStarts();
         }
     }
 
-    private void wrongPick(){
+    private void getNewChoiceWhoStarts(){
         System.out.println("Please enter: \n1 for Computer or \n2 for Player.");
         int choice = board.getScanner().nextInt();
         if(choice == 1) {
             computerFirstMove();
         }
         if(!(choice == 1) && !(choice == 2)){
-            wrongPick();
+            getNewChoiceWhoStarts();
         }
     }
 
@@ -57,19 +57,15 @@ public class Game {
     }
 
     private void playGame(){
-        String userInput;
         String validatedUserInput;
-        int columnInt = -1;
+        int columnInt;
         int rowInt;
         while (board.isRunning()){
-            System.out.println( "Make a move by entering the letter corresponding to your chosen column plus enter\n" +
-                                "and the corresponding number to your chosen row plus enter.\n" +
-                                "Please enter your move:");
-            userInput = board.getScanner().next();
-            validatedUserInput = validateInput(userInput);
+            this.flag = false;
+            validatedUserInput = takeCoordinates();
             columnInt = getColumnInt(validatedUserInput);
-            rowInt =  getRowInt(validatedUserInput);
-            Tile userTile = new Tile( columnInt, rowInt);
+            rowInt = getRowInt(validatedUserInput);
+            Tile userTile = new Tile( rowInt, columnInt);
             board.move(userTile, Player.USER);
             board.displayBoard();
             board.callMinimax(0, Player.COMPUTER);
@@ -86,15 +82,93 @@ public class Game {
         }
     }
 
-
-    public String validateInput(String userInput){
-        String twoChars;
-        if(!(userInput.length() == 2)){
-            twoChars = takeNewInput();
-        }else{
-            twoChars = userInput;
+    private String takeCoordinates() {
+        String result = null;
+        while(!this.flag){
+            System.out.println( "Make a move by entering the letter corresponding to your chosen column plus enter\n" +
+                    "and the corresponding number to your chosen row plus enter.\n" +
+                    "Please enter your move:");
+            String userInput = board.getScanner().next();
+            if(miracleFunction(userInput)){
+                result = userInput;
+                this.flag = true;
+            }
         }
-        return letterPlusDigit(twoChars);
+        return result;
+    }
+
+    //TODO validation should go to a separate class
+    private boolean miracleFunction(String userInput) {
+        if(isLengthTwoDigits(userInput)){
+            if(oneLetterOneDigit(userInput)){
+                if(rightOrder(userInput)){ //TODO it should recognize the coordinates in the wrong order too
+                    if(rightValues(userInput)){ //TODO user should not be able to move twice to the same field
+                        return true;
+                    }else{
+                        System.out.println("Listen, the coordinates are wrong. :(\n");
+                        return false;
+                    }
+                }else{
+                    if(rightValues(changeOrderOfChars(userInput))){
+                        System.out.println("Listen, you need to enter the letter followed by a digit. :(\n");
+                        return false;
+                    }else{
+                        System.out.println("Listen, the coordinates are wrong. :(\n");
+                        return false;
+                    }
+                }
+            }else{
+                System.out.println("Mate, you need to enter two characters. A *letter* and a *number*.\n");
+                return false;
+            }
+        }else{
+            System.out.println("Mate, you need to enter *two* characters. A letter and a number.\n");
+            return false;
+        }
+    }
+
+    private boolean rightValues(String userInput) {
+        String letter = String.valueOf(userInput.charAt(0));
+        String digit = String.valueOf(userInput.charAt(1));
+        if((letter.equals("a") || letter.equals("A")  ||
+            letter.equals("b") || letter.equals("B")  ||
+            letter.equals("c") || letter.equals("C")) &&
+           (Integer.parseInt(digit) == 1 ||
+            Integer.parseInt(digit) == 2 ||
+            Integer.parseInt(digit) == 3)){
+            return true;
+        }
+        return false;
+    }
+
+    private String changeOrderOfChars(String userInput) {
+        String letter = String.valueOf(userInput.charAt(1));
+        String digit = String.valueOf(userInput.charAt(0));
+        return letter + digit;
+    }
+
+    private boolean rightOrder(String userInput) {
+        if(Character.isLetter(userInput.charAt(0))){
+            return true;
+        }
+        return false;
+    }
+
+    private boolean oneLetterOneDigit(String userInput) {
+        //TODO can I reduce it to a single file?
+        if (Character.isLetter(userInput.charAt(0)) && Character.isDigit(userInput.charAt(1))) {
+            return true;
+        } else if(Character.isLetter(userInput.charAt(1)) && Character.isDigit(userInput.charAt(0))) {
+            return true;
+        }
+        return false;
+    }
+
+    private boolean isLengthTwoDigits(String userInput) {
+        if(userInput.length() == 2){
+            return true;
+        }
+        return false;
     }
 
     private String takeNewInput(){
@@ -106,40 +180,20 @@ public class Game {
         return userInput;
     }
 
-    private String letterPlusDigit(String twoChars){
-        String letter = null;
-        String digit = null;
-        if(Character.isLetter(twoChars.charAt(0)) && !Character.isLetter(twoChars.charAt(1))){
-            letter = Character.toString(twoChars.charAt(0));
-        }else if(Character.isLetter(twoChars.charAt(1)) && !Character.isLetter(twoChars.charAt(0))){
-            letter = Character.toString(twoChars.charAt(1));
-        }else{
-            letterPlusDigit(takeNewInput());
-        }
-        if(Character.isDigit(twoChars.charAt(0)) && !Character.isDigit(twoChars.charAt(1))){
-            digit = Character.toString(twoChars.charAt(0));
-        }else if(Character.isDigit(twoChars.charAt(1)) && !Character.isDigit(twoChars.charAt(0))){
-            digit = Character.toString(twoChars.charAt(1));
-        }else{
-            letterPlusDigit(takeNewInput());
-        }
-        return letter + digit;
-    }
-
     private int getColumnInt(String validatedUserInput){
         int columnInt = -1;
-        if(Character.compare(validatedUserInput.charAt(0), 'a') == 0 || Character.compare(validatedUserInput.charAt(0), 'A') == 0){
+        if(validatedUserInput.charAt(0) == 'a' || validatedUserInput.charAt(0) == 'A'){
             columnInt = 0;
-        }else if(Character.compare(validatedUserInput.charAt(0), 'b') == 0 || Character.compare(validatedUserInput.charAt(0), 'B') == 0){
+        }else if(validatedUserInput.charAt(0) == 'b' || validatedUserInput.charAt(0) == 'B'){
             columnInt = 1;
-        }else if(Character.compare(validatedUserInput.charAt(0), 'c') == 0 || Character.compare(validatedUserInput.charAt(0), 'C') == 0){
+        }else if(validatedUserInput.charAt(0) == 'c' || validatedUserInput.charAt(0) == 'C'){
             columnInt = 2;
         }
         return columnInt;
     }
 
     private int getRowInt(String validatedUserInput) {
-        return validatedUserInput.charAt(1) - 1;
+        return Integer.parseInt(String.valueOf(validatedUserInput.charAt(1))) -1;
     }
 
     private void checkStatus(){
